d50150686ca7704a848feab1a65427edb38e25a8
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-04-17 16:19:51 +0100

    host on Render

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy  
 
-  validates :title, presence: true
+  validates :title
   validates :content, presence: true
 end
